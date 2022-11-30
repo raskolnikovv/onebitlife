30fefaea8bec0e700a6b80cc372838e83ac5a520
Raskolnikovv <raskolnikovsete@gmail.com>
2022-11-30 20:02:37 -0300

    first commit

---
+++ src/Pages/Start/index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
+
+export default function Start() {
+  return (
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ alignItems: "center" }}>
+          <Image 
+            style={styles.logo} 
+            source={require("../../assets/icons/logo3.png")} 
+          />
+          <Text style={styles.description}>
+            Vamos transformar sua vida {`\n`} em jogo, buscando sempre {`\n`} o 
+            melhor nível.
+          </Text>
+        </View>
+      </ScrollView>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+});
---
+++ src/Routes/index.jsx
@@ -0,0 +1,7 @@
+import { useState } from 'react';
+
+import AllPages from "./AllPages";
+
+export default function Routes() {
+  return <AllPages />
+}
