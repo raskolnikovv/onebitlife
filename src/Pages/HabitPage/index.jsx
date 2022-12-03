96353d0ac44b020d27555d1c91d9a00c514c1abd
Raskolnikovv <raskolnikovsete@gmail.com>
2022-12-03 17:27:24 -0300

    Dropdown SelectFrequency

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -11,22 +11,24 @@ import {
 import { useNavigation } from "@react-navigation/native";
 
 import SelectHabit from "../../Components/HabitPage/SelectHabit";
+import SelectFrequency from "../../Components/HabitPage/SelectFrequency";
 
 export default function HabitPage({ route }) {
   const navigation = useNavigation();
   const [habitInput, setHabitInput] = useState();
+  const [frequencyInput, setFrequencyInput] = useState();
 
   const { create, habit } = route.params;
 
   return (
-    <View style={styles.container}> 
+    <View style={styles.container}>
       <ScrollView>
         <View>
           <TouchableOpacity
             style={styles.backPageBtn}
             onPress={() => navigation.goBack()}
           >
-            <Image  
+            <Image
               source={require("../../assets/icons/arrowBack.png")}
               style={styles.arrowBack}
             />
@@ -41,11 +43,16 @@ export default function HabitPage({ route }) {
             <Text style={styles.inputText}>Hábito</Text>
             <SelectHabit habit={habit} habitInput={setHabitInput} />
 
+            <Text style={styles.inputText}>Frequência</Text>
+            <SelectFrequency
+              habitFrequency={habit?.habitFrequency}
+              frequencyInput={setFrequencyInput}
+            />
           </View>
         </View>
       </ScrollView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
